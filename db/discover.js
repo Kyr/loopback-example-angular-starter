eb28cbf2771b5897f2f759d05105f619c3378261
pavel.kruchina <pavel.kruchina@gmail.com>
2014-10-18 13:54:08 +0300

    example of sql query

--- db/discover.js
+++ db/discover.js
@@ -1,52 +1,6 @@
 var app = require('../server/server.js');
 var dataSource = app.dataSources.mysql;
-
-/*dataSource.discoverSchema('users', {owner: 'inf'}, function (err, schema) {
-    console.log(JSON.stringify(schema, null, '  '));
-});
-*/
-var ds = app.dataSources.mysql;
-//var book = app.models.book;
-var book = app.models.user;
-//ds.createModel(, schema_v1.properties, schema_v1.options);
-  
-/*ds.automigrate(function () {
-  ds.discoverModelProperties('CUSTOMER_TEST', function (err, props) {
-    console.log(props);
-  });
-});
-*/
-var books = [ 
-    { name: "n1",
-      id: "1"
-    }
-];
-
-var count = books.length;
-dataSource.automigrate('user', function (err) {dataSource.disconnect();});
-/*dataSource.automigrate('book', function (err) {
-  books.forEach(function(act) {
-    book.create(act, function(err, result) {
-      if(!err) {
-        console.log('Record created:', result);
-        count--;
-        if(count === 0) {
-          console.log('done');
-          dataSource.disconnect();
-        }
-      }
-    });
-  });
-});*/
-//dataSource.createModel("Todo",)
-/*dataSource.discoverAndBuildModels('users', {owner: 'inf'}, function (err, models) {
-    models.DBUser.find(function (err, act) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(act);
-        }
-        dataSource.disconnect();
-    });
-});
-*/
+dataSource.connector.query(
+	'insert into book set name="asd"', 
+	function(){dataSource.disconnect();}
+);
